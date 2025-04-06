ecb374f181f6b926b7b29faea666f5bf49e54beb
sivasaikum <sivasaikumarallampati@gmail.com>
2025-04-07 00:51:00 +0530

    terrfaorm alb

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -25,5 +25,5 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
 resource "aws_ssm_parameter" "app_alb_sg_id" {
     name = "/${var.project}/${var.environment}/app_alb_sg_id"
     type = "String"
-    value = module.bastion_sg.sg_id
+    value = module.app_alb_sg.sg_id
 }

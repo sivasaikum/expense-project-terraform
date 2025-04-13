9388a5c9ba7d851175568857f32cdb283fc214b7
sivasaikum <sivasaikumarallampati@gmail.com>
2025-04-13 14:36:55 +0530

    terraform project

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -53,6 +53,17 @@ module "app_alb_sg" {
 
 }
 
+module "vpn_sg" {
+    source = "git::https://github.com/sivasaikum/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_name = "vpn"
+    sg_desc = "creating a security group for VPN in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+
+}
+
 # APP ALB is accepting traffic from bastion
 resource "aws_security_group_rule" "app_alb_bastion" {
   type              = "ingress"
@@ -67,6 +78,42 @@ resource "aws_security_group_rule" "bastion_public" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_22" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
   protocol          = "tcp"
   cidr_blocks      = ["0.0.0.0/0"]
   security_group_id = module.bastion_sg.sg_id
